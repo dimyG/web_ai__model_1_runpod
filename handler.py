8815ffdef9a98b55016f6f45e9d817d2b8c8c6ae
dimyG <dimgeows@gmail.com>
2023-02-11 17:16:44 +0200

    initial working runpod container

---
+++ handler.py
@@ -0,0 +1,46 @@
+import runpod
+import os
+import io
+import base64
+from ml import load_pipeline, img_from_prompt
+# from main import generate_img
+
+# We read the defined cache dir from the env file, and then we use the cache_dir parameter to use it.
+# Using the cache_dir parameter could be avoided if we set the HUGGINGFACE_HUB_CACHE environment variable on the shell.
+# But it doesn't work just by adding it in the env file, so we read it and then use it as the cache_dir parameter.
+huggingface_hub_cache = os.environ.get("HUGGINGFACE_HUB_CACHE")
+
+model1 = "CompVis/stable-diffusion-v1-4"
+model2 = "stabilityai/stable-diffusion-2"
+# Runpod advice: load your model(s) into vram here
+pipe_initial = load_pipeline(model2, cache_dir=huggingface_hub_cache)
+
+
+def handler(event):
+    """
+    event payload example
+    {
+        'delayTime': 2534,
+        'id': '2a16b881-830f-4d14-af5b-f7db7c0a96fc',
+        'input': {
+            'prompt': 'some text.'
+        },
+        'status': 'IN_PROGRESS'
+    }
+    """
+    print(event)
+    # Runpod requirement: If the image is larger than 2MB, store it in a blob store and return an url to it.
+    # image = generate_img(event['input']['prompt'], pipe=pipe_initial)
+    # return image
+    image = img_from_prompt(event['input']['prompt'], pipe_initial, height=256, width=256, guidance_scale=7.5, num_inference_steps=10)
+    memory_stream = io.BytesIO()
+    image.save(memory_stream, format='PNG')  # write the image to memory instead of the disk, and return it from there
+    memory_stream.seek(0)
+    # Convert the contents of the memory stream to a base64 encoded string
+    encoded_string = base64.b64encode(memory_stream.getvalue()).decode('utf-8')
+    return encoded_string
+
+
+runpod.serverless.start({
+    "handler": handler
+})
